460f20cbd0800aaf9fe006e9ae9401fe8aa17a35
Santyru <santiago0988@hotmail.com>
2023-05-09 15:04:56 -0300

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'eval-source-map',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: false,
   },
   module: {
     rules: [
@@ -13,11 +19,12 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-
         type: 'asset/resource',
-
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },

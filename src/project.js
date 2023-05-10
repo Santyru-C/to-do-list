2a218d51246838207a23f681d7e74b98b4cc40bf
Santyru <santiago0988@hotmail.com>
2023-05-10 17:39:51 -0300

    add getter function for tasklist property

--- src/project.js
+++ src/project.js
@@ -6,15 +6,19 @@ export default class Project {
     this.taskList = [];
   }
 
-  add_new_task(title, description, dueDate = 'No Date', priority = 'Low') {
+  addNewTask(title, description, dueDate = 'No Date', priority = 'Low') {
     const newTask = new Task(title, description, dueDate, priority);
     this.taskList.push(newTask);
   }
 
-  remove_task(task) {
+  removeTask(task) {
     if (this.taskList.includes(task)) {
       const indexToRemove = this.taskList.indexOf(task);
       this.taskList.splice(indexToRemove, 1);
     } else { console.log('ERROR: That Item Does Not Exist'); }
+  }
+
+  listTasks() {
+    return this.taskList;
   }
 }

ab55fec10d746b85af2611532c9a12102a044509
Santyru <santiago0988@hotmail.com>
2023-05-10 17:11:59 -0300

    add task removal function

--- src/project.js
+++ src/project.js
@@ -10,4 +10,11 @@ export default class Project {
     const newTask = new Task(title, description, dueDate, priority);
     this.taskList.push(newTask);
   }
+
+  remove_task(task) {
+    if (this.taskList.includes(task)) {
+      const indexToRemove = this.taskList.indexOf(task);
+      this.taskList.splice(indexToRemove, 1);
+    } else { console.log('ERROR: That Item Does Not Exist'); }
+  }
 }

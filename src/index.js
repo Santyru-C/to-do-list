6975f7829e6b9ad2657a0d0ae13ccf3cbd5d616e
Santyru <santiago0988@hotmail.com>
2023-05-10 16:49:11 -0300

    import proyect module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+import Project from './project';
+
+const Project1 = new Project('Deportes');
+console.log(Project1);
+Project1.add_new_task('Jugar al teto', 'e.e', 'Hoy mismo', 'Alta');
+Project1.add_new_task('Gimnasia', 'g');
+console.log(Project1.taskList);

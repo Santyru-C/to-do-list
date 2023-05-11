0ecc2d89f8122f227c2e91ad0ad97cd08cc0e240
Santyru <santiago0988@hotmail.com>
2023-05-11 17:55:02 -0300

    add DOM manipulation function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import App from './app';
 
 const todoList = new App(); // set it as an IIFE later
-todoList.add_new_project('juancarlos');
+todoList.addNewProject('juancarlos');
+todoList.addNewProject('projecto2');
+const project1 = todoList.projectList[0];
+const project2 = todoList.projectList[1];
+project1.addNewTask('1', '2');
+project2.addNewTask('3', '4');
+console.log(todoList.getAllTasks());
 console.log(todoList);
+
+const menuBtn = document.getElementById('menu-btn');
+const navContainer = document.getElementsByClassName('nav-container')[0];
+
+function toggleNavContainer() {
+  const { classList } = navContainer;
+  const classArray = Array.from(classList);
+  (classArray.includes('hidden')) ? classList.remove('hidden') : classList.add('hidden');
+}
+menuBtn.addEventListener('click', () => {
+  toggleNavContainer();
+});

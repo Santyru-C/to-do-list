7a19463a4f064a64e036e7e174155816cc545447
Santyru <santiago0988@hotmail.com>
2023-05-12 16:15:35 -0300

    add DomManipulator module

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import App from './app';
 
 const todoList = new App(); // set it as an IIFE later
-todoList.addNewProject('juancarlos');
-todoList.addNewProject('projecto2');
-const project1 = todoList.projectList[0];
-const project2 = todoList.projectList[1];
-project1.addNewTask('1', '2');
-project2.addNewTask('3', '4');
-console.log(todoList.getAllTasks());
-console.log(todoList);
 
-const menuBtn = document.getElementById('menu-btn');
-const navContainer = document.getElementsByClassName('nav-container')[0];
+const DomManipulator = (() => {
+  const menuBtn = document.getElementById('menu-btn');
+  const newProjectButton = document.getElementById('new-project-btn');
+  const navContainer = document.getElementsByClassName('nav-container')[0];
+  const newProjectModal = document.getElementsByClassName('new-project-modal')[0];
 
-function toggleNavContainer() {
-  const { classList } = navContainer;
-  const classArray = Array.from(classList);
-  (classArray.includes('hidden')) ? classList.remove('hidden') : classList.add('hidden');
-}
-menuBtn.addEventListener('click', () => {
-  toggleNavContainer();
-});
+  const toggleContainer = (container) => {
+    const { classList } = container;
+    const classArray = Array.from(classList);
+    (classArray.includes('hidden')) ? classList.remove('hidden') : classList.add('hidden');
+  };
+
+  menuBtn.addEventListener('click', () => {
+    toggleContainer(navContainer);
+  });
+
+  newProjectButton.addEventListener('click', () => {
+    toggleContainer(newProjectModal);
+  });
+})();
